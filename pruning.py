dd7f94f8fb8200a1c9dadf181c7b6871a42cfcb2
rammalahmad <rammal.ahmad@outlook.com>
2025-02-21 21:22:11 +0100

    progress

--- pruning.py
+++ pruning.py
@@ -1,6 +1,20 @@
 import torch.nn as nn
 from transformers.models.gpt2.modeling_gpt2 import GPT2Block, GPT2Attention, GPT2MLP
 
+def prune_layer(layer: nn.Module, idx, device, dim=0) -> None:
+    num_neurons = idx.size(0)
+    if dim == 0:
+        new_layer = nn.Linear(num_neurons, layer.out_features).to(device)
+        new_layer.weight.data = layer.weight.data[idx, :]
+        new_layer.bias.data = layer.bias.data
+    elif dim == 1:
+        new_layer = nn.Linear(layer.in_features, num_neurons).to(device)
+        new_layer.weight.data = layer.weight.data[:, idx]
+        new_layer.bias.data = layer.bias.data
+    else:
+        raise ValueError("Invalid dimension")
+    return new_layer
+    
 
 def prune_mlp(model, mult_factor: float = 4.0) -> None:
     # goal: trim the width of the MLP layers in the transformer blocks
@@ -34,73 +48,52 @@ def prune_mlp(model, mult_factor: float = 4.0) -> None:
             module.mlp.c_fc = new_fc
             module.mlp.c_proj = new_proj
 
-    return model
 
+class CausalSelfAttention(nn.Module):
 
-
-class MultiHeadAttention(nn.Module):
-    """implements multi-headed masked self-attention using tensor operations"""
-
-    def __init__(self, num_heads, head_size, n_embd, device, block_size, dropout=0.2):
+    def __init__(self, config):
         super().__init__()
-        gen = torch.Generator(device=device)
-        gen.manual_seed(42)
-
-        self.num_heads = num_heads
-        self.n_embd = n_embd
-        self.device = device
-        self.block_size = block_size
-        self.dropout = dropout
-        self.head_size = head_size
-
-        key = (
-            torch.randn(num_heads, n_embd, head_size, generator=gen, device=device)
-            * (n_embd * num_heads) ** -0.5
-        )
-        query = (
-            torch.randn(num_heads, n_embd, head_size, generator=gen, device=device)
-            * (n_embd * num_heads) ** -0.5
-        )
-        value = (
-            torch.randn(num_heads, n_embd, head_size, generator=gen, device=device)
-            * (n_embd * num_heads) ** -0.5
-        )
-
-        self.key = nn.Parameter(key)
-        self.query = nn.Parameter(query)
-        self.value = nn.Parameter(value)
-
-        self.proj = nn.Linear(n_embd, n_embd)
-
-        self.dropout = nn.Dropout(dropout)
-
-        # (B, T, n_embd) x (num_heads, n_embd, head_size) --> (B, num_heads, T, head_size)
-        self.register_buffer(
-            "tril", torch.tril(torch.ones(num_heads, block_size, block_size))
-        )
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
-        B, T, C = x.shape
-        x = x.unsqueeze(1)  # (batch_size, 1, context_length, n_embd)
-        k = x @ self.key  # (batch_size, num_heads, context_length, head_size)
-        q = x @ self.query  # (batch_size, num_heads, context_length, head_size)
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
-        wei = (
-            q @ k.transpose(-2, -1) * (self.head_size**-0.5)
-        )  # (bs, nh, cl, hs) x (bs, nh, hs, cl) -> (bs, nh, cl, cl)
-        wei = wei.masked_fill(self.tril[:, :T, :T] == 0, float("-inf"))
-        wei = F.softmax(wei, dim=-1)  # (bs, nh, cl, cl)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
-        v = x @ self.value  # (bs, 1, cl, ne) x (nh, ne, hs) -> (bs, nh, cl, hs)
-        out = wei @ v  # (bs, nh, cl, cl) x (bs, nh, cl, hs) -> (bs, nh, cl, hs)
-        out = out.permute(0, 2, 1, 3)  # (bs, cl, nh, hs)
-        out = out.reshape(
-            out.shape[0], out.shape[1], self.n_embd
-        )  # (bs, cl, n_embd) = (B, T, C)
 
-        out = self.proj(out)
-        out = self.dropout(out)
-        return out
+        # manual implementation of attention
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+
 
 def prune_heads(model, n: list[int] | float) -> None:
     # goal: trim the attention heads' layer weights using the same approach as the `prune_neurons`
