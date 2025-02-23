5d308493f3cc7b1f28b5f7a8e5aa60a3ed482c80
rammalahmad <rammal.ahmad@outlook.com>
2025-02-23 12:00:30 +0100

    simpler function to calculate model size

--- pruning_utils.py
+++ pruning_utils.py
@@ -6,16 +6,7 @@ from transformers.pytorch_utils import Conv1D
 
 def model_size(model):
     """Computes the number of parameters in a model."""
-    num_params = 0
-    sd = model.state_dict()
-
-    for k, v in sd.items():
-        # Skip counting the output projection weights since it is tied to the input embeddings
-        if k == "lm_head.weight":
-            continue
-        num_params += v.numel()
-
-    print(f"Corrected Total Parameters: {num_params:,}")
+    return sum(t.numel() for t in model.parameters())
     
 def pruned_layer(layer: nn.Module, idx, device, dim=0) -> nn.Module:
     """
